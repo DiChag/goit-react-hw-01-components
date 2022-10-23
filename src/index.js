2e262eb5b444e81987cefeea244f8cfb5be8088d
DiChag <chagindmitry1@gmail.com>
2022-10-23 23:38:23 +0200

    add-friends

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ import user from 'data/user.json';
 import { Statistics } from './components/statistics/Statistics';
 import data from './data/data.json';
 
+import { FriendList } from './components/friend/FriendList';
+import friends from './data/friends.json';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App>
@@ -20,6 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </App>
   </React.StrictMode>
 );
