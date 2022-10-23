d3223fca3109779ebe54d38468b901280fb35b88
DiChag <chagindmitry1@gmail.com>
2022-10-23 21:14:52 +0200

    statistics

--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import { Profile } from 'components/profile/Profile';
+import user from 'data/user.json';
+
+import { Statistics } from './components/statistics/Statistics';
+import data from './data/data.json';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+    </App>
   </React.StrictMode>
 );
