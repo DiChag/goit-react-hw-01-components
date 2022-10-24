542833783453a580120e9af5f04d5b679831f7c7
DiChag <chagindmitry1@gmail.com>
2022-10-24 13:03:18 +0200

    fix-after-mentor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = ({ children }) => {
+import { Profile } from './profile/Profile';
+import user from '../data/user.json';
+
+import { Statistics } from './statistics/Statistics';
+import data from '../data/data.json';
+
+import { FriendList } from './friend/FriendList';
+import friends from '../data/friends.json';
+
+import { Transaction } from './transaction/Transaction';
+import transactions from '../data/transactions.json';
+
+export const App = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      {children}
-    </div>
+    <main className="main">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <Transaction items={transactions} />
+    </main>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,31 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-import { Profile } from 'components/profile/Profile';
-import user from 'data/user.json';
-
-import { Statistics } from './components/statistics/Statistics';
-import data from './data/data.json';
-
-import { FriendList } from './components/friend/FriendList';
-import friends from './data/friends.json';
-
-import { Transaction } from './components/transaction/Transaction';
-import transactions from './data/transactions.json';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <Transaction items={transactions} />
-    </App>
+    <App />
   </React.StrictMode>
 );
